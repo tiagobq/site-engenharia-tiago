8722dbc754297d0a4ec5227b0fe50adc5d96b734
Tiago Sousa <159965545+tiagobq@users.noreply.github.com>
2026-03-22 20:05:12 -0300

    Update Home.jsx

--- site-engenharia-tiago/src/Home.jsx
+++ site-engenharia-tiago/src/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./home.css"; 
-import banner from "./assets/banner.webp"
+import bannerImg from "./assets/banner.webp"
 
 <img src={banner} alt="Next Engenharia Mecânica" />
 
